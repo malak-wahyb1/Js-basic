545cb96e5dd04f663f9d8743b7ebc89688180e3a
malak-wahyb <malakwahyb537@gmail.com>
2022-12-13 16:13:08 +0200

    loop

---
+++ problem3.js
@@ -0,0 +1,11 @@
+// var Name=prompt("what is you name");
+// console.log(a)
+   function show(a){
+    var a=document.getElementById("num").value;
+    var cal=0;
+    for (i=1;i<=a;i++) {
+       cal+=i;
+      }
+
+      alert(cal) 
+}
