30e8cd378aa97d77a89cd14741683cdb772130f9
malak-wahyb <malakwahyb537@gmail.com>
2022-12-13 15:56:44 +0200

    condition

---
+++ problem2.js
@@ -0,0 +1,12 @@
+// var Name=prompt("what is you name");
+function show(){
+    var a=document.getElementById("Name").value;
+    if (a=="Alice") {
+        alert("hello Alice")
+    } else if(a=="bob"){
+         alert("hello bob")
+    }
+    else{
+        alert("Hello Stranger")
+    }
+}
